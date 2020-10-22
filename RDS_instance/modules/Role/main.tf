2f98f79c4691254225993fa0aaaa9e60c843e954
nmccrawavl <48925950+nmccrawavl@users.noreply.github.com>
2020-10-22 14:43:15 -0400

    Update main.tf

--- RDS_instance/modules/Role/main.tf
+++ RDS_instance/modules/Role/main.tf
@@ -1,12 +1,12 @@
 
 resource "aws_iam_role" "rds_enhanced_monitoring" {
-  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring[0].json
+  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
   name_prefix        = "rds-enhanced-monitoring"
 }
 
 resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-  role       = aws_iam_role.rds_enhanced_monitoring[0].name
+  role       = aws_iam_role.rds_enhanced_monitoring.name
 }
 
 data "aws_iam_policy_document" "rds_enhanced_monitoring" {
