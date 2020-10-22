2245bbefe9fecc5b97b0e6df1715d985c0816dbf
nmccrawavl <48925950+nmccrawavl@users.noreply.github.com>
2020-10-22 15:00:01 -0400

    Update main.tf

--- RDS_instance/modules/SecurityGroup/main.tf
+++ RDS_instance/modules/SecurityGroup/main.tf
@@ -3,9 +3,9 @@ resource "aws_security_group" "sg_on_rds_instance" {
   name        = var.security_group_name
   vpc_id      = var.vpc_id
 
-  ingress = var.ingress
+  ingress = {var.ingress}
 
-  egress = var.egress
+  egress ={ var.egress}
 
   tags = merge(
   {
