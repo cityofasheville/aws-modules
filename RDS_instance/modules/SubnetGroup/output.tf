1eaa30abec7c85235854c6d540c507cef1914a4f
nmccrawavl <48925950+nmccrawavl@users.noreply.github.com>
2020-10-22 14:45:31 -0400

    Update output.tf

--- RDS_instance/modules/SubnetGroup/output.tf
+++ RDS_instance/modules/SubnetGroup/output.tf
@@ -1,11 +1,11 @@
 output "name" {
-  value = aws_db_subnet_group.mod.name
+  value = aws_db_subnet_group.mod[0].name
 }
 
 output "id" {
-  value = aws_db_subnet_group.mod.id
+  value = aws_db_subnet_group.mod[0].id
 }
 
 output "arn" {
-  value = aws_db_subnet_group.mod.arn
+  value = aws_db_subnet_group.mod[0].arn
 }
