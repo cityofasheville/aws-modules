9cfce1fff973983fb33d369261be9e5fa3c33672
Nancie McCraw <nmccraw@ashevillenc.gov>
2020-11-12 14:22:33 -0500

    corrected rds proxy

--- RDS_Db_Proxy/main.tf
+++ RDS_Db_Proxy/main.tf
@@ -39,7 +39,6 @@ resource "aws_db_proxy_default_target_group" "target_group" {
     init_query                   = "SET x=1, y=2"
     max_connections_percent      = 100
     max_idle_connections_percent = 50
-    session_pinning_filters      = ["EXCLUDE_VARIABLE_SETS"]
   }
 }
 
